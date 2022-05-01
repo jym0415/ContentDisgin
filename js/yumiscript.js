24260f36706adf6c0fd03f9accf0c3fe3795574a
jym0415 <jym9504@gmail.com>
2022-05-01 19:32:24 +0900

    hover,swiper삽입

--- js/yumiscript.js
+++ js/yumiscript.js
@@ -1,6 +1,17 @@
-$(document).ready(function(){
+window.onload = function(){
     AOS.init();
+    var swiper = new Swiper("#EtSwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+          scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: true,
+          },
 
-
-
-});
+      });
+};
